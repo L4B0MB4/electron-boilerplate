5b2fc9bc8bc37c9d59e3e483b7bd043591aa85b2
L4B0MB4 <metalfan65@gmail.com>
2019-05-15 21:34:48 +0200

    removed usless line

--- public/electron.js
+++ public/electron.js
@@ -2,4 +2,5 @@
  * I dont like to place my main.js for electron here - so I´m just requiring it.
  * Maybe there´s a better way but I guess one had to eject react
  */
-process.env.NODE_ENV === "development" ? require("../build/backend/main") : require("./backend/main");
+
+require("./backend/main");
